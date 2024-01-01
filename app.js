b85c7621a18d1dfeeec79135b2310ca568bd7b71
samuel21695 <samuel21695@gmail.com>
2024-01-01 16:26:06 +0900

    ✨ function ReactTenDashOne-1

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 import { useState } from "react";
+
+function ReactTenDashOne() {
+  const stateWork = [];
+  stateWork.length = 10;
+  stateWork.fill(false);
+  console.log(stateWork); // false로 10개의 원소가 채워진 배열 stateWork
+  const [packs, setPacks] = useState(stateWork); // 각 팩의 열림 상태를 관리
+}
